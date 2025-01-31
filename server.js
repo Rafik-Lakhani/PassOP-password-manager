5cc015e83480dcba2c6e63983a057c989e66f693
Rafik-Lakhani <lakhanirafik111@gmail.com>
2025-01-31 22:56:17 +0530

    change in vercle.json file

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ var timeout = false;
 app.use(cookieParser());
 app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
 
